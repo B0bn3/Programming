bbbb18a42331a226fd62411d8d8289846611ec8f
B0bn3 <4rohangala@gmail.com>
2025-04-08 13:48:37 +1200

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+DATABASE = 'cars.db'
+
+def print_all_car():
+    speed = input('What speed: ')
+    with sqlite3.connect(DATABASE) as db:
+        db = sqlite3.connect('cars.db')
+        cursor = db.cursor()
+        sql = "SELECT car_name,top_speed FROM car WHERE top_speed > ?;"
+        cursor.execute(sql,(speed,))
+        results = cursor.fetchall()
+        #Print them nicely
+
+        for car in results:
+            print(f"Car: {car[0]} Top Speed : {car[1]}")
+
+
+
+if __name__== '__main__':
+    print_all_car()
