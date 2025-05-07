234cd5a08685e335aace9be7a5f4437dd275d669
B0bn3 <4rohangala@gmail.com>
2025-05-08 09:25:13 +1200

    Print_top_speed

--- test.py
+++ test.py
@@ -16,8 +16,33 @@ def print_all_car():
         for car in results:
             print(f"{car[2]:34} {car[1]:<15} {car[3]}")
             #print(f"name: {car[2]} top speed : {car[1]} manufactor : {car[3]}")
+        db.close
+
+def print_top_speed():
+    with sqlite3.connect(DATABASE) as db:
+        db = sqlite3.connect('CARS_FINAL.db')
+        cursor = db.cursor()
+        sql = "SELECT top_speed,name FROM CARS ORDER BY top_speed DESC;"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        #Print them nicely
+        print(f"top_speed")
+
+        for car in results:
+            print(f"{car[0]:}")
+            #print(f"name: {car[2]} top speed : {car[1]} manufactor : {car[3]}")
+        db.close
 
 
-
-if __name__== '__main__':
-    print_all_car()
+while True:
+    user_input = input("\nWhat would like to do.\n1. Print all cars\n2. Print top_speed\n7. exit\n")
+    if user_input == "1":
+        print_all_car()
+    elif user_input == "2":
+        print_top_speed()
+    elif user_input == "3":
+        pass
+    elif user_input == "7":
+        break
+    else:
+        print("Thats not an option\n")
